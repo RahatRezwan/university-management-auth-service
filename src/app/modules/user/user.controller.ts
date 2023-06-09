c1c659a188e59f56c50d492fee9f746d18162762
RahatRezwan <rahat15-10971@diu.edu.bd>
2023-06-09 16:57:20 +0600

    create user zod schema

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,8 +1,24 @@
 import { RequestHandler } from 'express'
+import { z } from 'zod'
 import { UserService } from './user.service'
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
+    //req-validation
+    //body -> object
+    //data -> object
+
+    const createUserZodSchema = z.object({
+      body: z.object({
+        role: z.string({
+          required_error: 'Role is required',
+        }),
+        password: z.string().optional(),
+      }),
+    })
+
+    await createUserZodSchema.parseAsync(req)
+
     const { user } = req.body
     const result = await UserService.createUserToDB(user)
     res.status(200).json({
