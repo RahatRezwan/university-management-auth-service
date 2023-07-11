f3608583f83339b3890ae829b0be1dde875f1235
RahatRezwan <rahat15-10971@diu.edu.bd>
2023-07-11 06:00:42 +0600

    [DONE] Create admin and management department module

---
+++ src/app/modules/admin/admin.constant.ts
@@ -0,0 +1,23 @@
+export const adminFilterableFields = [
+  'searchTerm',
+  'id',
+  'gender',
+  'bloodGroup',
+  'email',
+  'contactNo',
+  'emergencyContactNo',
+  'managementDepartment',
+  'designation',
+];
+
+export const adminSearchableFields = [
+  'email',
+  'contactNo',
+  'emergencyContactNo',
+  'name.firstName',
+  'name.lastName',
+  'name.middleName',
+];
+
+export const genders = ['male', 'female'];
+export const bloodGroups = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
---
+++ src/app/modules/admin/admin.interface.ts
@@ -0,0 +1,40 @@
+import { Model, Types } from 'mongoose';
+import { IBloodGroup, IGender } from '../../../interfaces/common';
+import { IManagementDepartment } from '../managementDepartment/managementDepartment.interface';
+
+export type UserName = {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+};
+
+export type IAdmin = {
+  id: string;
+  name: UserName;
+  profileImage: string;
+  dateOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  gender?: IGender;
+  permanentAddress?: string;
+  presentAddress?: string;
+  bloodGroup?: IBloodGroup;
+
+  managementDepartment: Types.ObjectId | IManagementDepartment;
+  designation: string;
+};
+
+export type AdminModel = Model<IAdmin, Record<string, unknown>>;
+
+export type IAdminFilters = {
+  searchTerm?: string;
+  id?: string;
+  email?: string;
+  contactNo?: string;
+  emergencyContactNo?: string;
+  gender?: IGender;
+  bloodGroup?: IBloodGroup;
+  managementDepartment?: string;
+  designation?: string;
+};
